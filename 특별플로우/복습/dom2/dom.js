b0a9f17449012dae25fe8fae774464b99a7d2d10
mugi7794 <mugi7794@naver.com>
2023-07-28 22:37:07 +0900

    dom2

--- 특별플로우/복습/dom2/dom.js
+++ 특별플로우/복습/dom2/dom.js
@@ -3,7 +3,9 @@ console.log(mockPost);
 
 const $postDetail = document.querySelector('#post-detail');
 const $repliesList = document.querySelector('#replies-list');
-
+const $button = document.querySelector("button");
+const $comment = document.querySelector("#comment")
+const $user = document.querySelector("#user");
 /* 
     import(참조)한 json data를
     게시글 상세와 댓글창에 나타내고 게시글 객체의 상세 내용은 console.log로 출력해두었습니다
@@ -12,3 +14,48 @@ const $repliesList = document.querySelector('#replies-list');
 
     삭제 및 수정기능은 본인의 자유로 구현하시면 됩니다 :)
 */
+
+function mockList() {
+
+    const ul = document.createElement("ul")
+    
+
+    const li = document.createElement("li")
+    li.innerHTML=`title : ${mockPost.post.title}
+    content : ${mockPost.post.content}
+    User : ${mockPost.post.User.nickName}`
+
+    ul.appendChild(li)
+    $postDetail.appendChild(ul)
+
+}
+
+mockList()
+
+function mockComment(){
+
+    for(let i=0; i<mockPost.post.Replies.length; i++){
+    const li = document.createElement("li")
+    li.innerHTML=`글쓴이 : ${mockPost.post.Replies[i].User.nickName}
+    내용 : ${mockPost.post.Replies[i].content}`
+    $repliesList.appendChild(li)
+    }
+
+}
+
+mockComment()
+
+$button.addEventListener("click", addComment)
+
+function addComment(){
+    console.log($comment.value)
+
+    const li = document.createElement("li")
+    li.innerHTML=`글쓴이 : ${$user.value}
+    내용 : ${$comment.value}
+    `
+
+    $repliesList.appendChild(li)
+    $user.value=""
+    $comment.value=""
+}
